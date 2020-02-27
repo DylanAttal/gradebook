5e14cd79d21462abde48530a810c19c321ae0072
Dylan Attal <attal.dylan@gmail.com>
2020-02-27 16:02:38 -0500

    puts logic into Book class

--- Book.cs
+++ Book.cs
@@ -1,18 +1,43 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
 {
   public class Book
   {
-    public Book()
+    public Book(string name)
     {
       grades = new List<double>();
+      this.name = name;
     }
-    List<double> grades = new List<double>();
+    private List<double> grades = new List<double>();
+    private string name;
 
     public void AddGrade(double grade)
     {
       grades.Add(grade);
+    }
+
+    public void ShowStatistics()
+    {
+      var sum = 0.0;
+      var highestGrade = double.MinValue;
+      var lowestGrade = double.MaxValue;
+      foreach (var number in grades)
+      {
+        sum += number;
+        if (number > highestGrade)
+        {
+          highestGrade = number;
+        }
+        lowestGrade = Math.Min(number, lowestGrade);
+      }
+      var average = sum / grades.Count;
+      // N1 is formatting the number so that it only has one decimal place.
+      Console.WriteLine($"The sum of all grades is {sum:N1}.");
+      Console.WriteLine($"The average grade is {average:N1}.");
+      Console.WriteLine($"The highest grade is {highestGrade:N1}.");
+      Console.WriteLine($"The lowest grade is {lowestGrade:N1}.");
     }
   }
 }
--- Program.cs
+++ Program.cs
@@ -7,24 +7,11 @@ namespace GradeBook
   {
     static void Main(string[] args)
     {
-      Book book2 = new Book();
-      book2.AddGrade(90.1);
-
-      var book = new Book();
+      var book = new Book("Dylan's Grade Book");
       book.AddGrade(89.1);
-
-      var grades = new List<double>() { 12.7, 10.3, 6.11 };
-      grades.Add(56.1);
-
-      var sum = 0.0;
-      foreach (var number in grades)
-      {
-        sum += number;
-      }
-      var average = sum / grades.Count;
-      // N1 is formatting the number so that it only has one decimal place.
-      Console.WriteLine($"The sum of all grades is {sum:N1}.");
-      Console.WriteLine($"The average grade is {average:N1}.");
+      book.AddGrade(90.1);
+      book.AddGrade(77.5);
+      book.ShowStatistics();
     }
   }
 }
