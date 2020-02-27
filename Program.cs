947af6c9c4da20860fc78db33f3cb531c2028a22
Dylan Attal <attal.dylan@gmail.com>
2020-02-27 09:05:11 -0500

    adds up grades using a List

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,9 +7,11 @@ namespace GradeBook
   {
     static void Main(string[] args)
     {
-      var numbers = new[] { 12.7, 10.3, 6.11 };
+      var grades = new List<double>() { 12.7, 10.3, 6.11 };
+      grades.Add(56.1);
+
       var result = 0.0;
-      foreach (double number in numbers)
+      foreach (var number in grades)
       {
         result += number;
       }
