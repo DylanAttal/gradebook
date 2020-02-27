f8ea25d45b5cd2ca2c875fa00d0f488a97b40368
Dylan Attal <attal.dylan@gmail.com>
2020-02-27 09:09:57 -0500

    calculates average of grades

--- Program.cs
+++ Program.cs
@@ -10,12 +10,15 @@ namespace GradeBook
       var grades = new List<double>() { 12.7, 10.3, 6.11 };
       grades.Add(56.1);
 
-      var result = 0.0;
+      var sum = 0.0;
       foreach (var number in grades)
       {
-        result += number;
+        sum += number;
       }
-      Console.WriteLine(result);
+      var average = sum / grades.Count;
+      // N1 is formatting the number so that it only has one decimal place.
+      Console.WriteLine($"The sum of all grades is {sum:N1}.");
+      Console.WriteLine($"The average grade is {average:N1}.");
 
       if (args.Length > 0)
       {
